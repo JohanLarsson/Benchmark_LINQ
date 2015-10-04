27e02edb8d0e4552397ed8208b18fe509219e617
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-04 17:16:10 +0200

    Add select benchmark.

--- Benchmark/Runs.cs
+++ Benchmark/Runs.cs
@@ -16,5 +16,16 @@ namespace Benchmark
                                 .Sum();
             Console.WriteLine($"{sum}: {sw.ElapsedMilliseconds} ms");
         }
+
+        [Test]
+        public void FilterDivideAndSum()
+        {
+            var sw = Stopwatch.StartNew();
+            var sum = Enumerable.Range(0, 1000000)
+                                .Where(x => x % 1000 == 0)
+                                .Select(x => x / 3.0)
+                                .Sum();
+            Console.WriteLine($"{sum}: {sw.ElapsedMilliseconds} ms");
+        }
     }
 }
