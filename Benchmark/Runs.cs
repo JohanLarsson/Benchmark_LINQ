e45af3f633617d31ab84e33764a5140576ab5bad
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-04 15:30:46 +0200

    Initial

---
+++ Benchmark/Runs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Benchmark
+{
+    public class Runs
+    {
+        [Test]
+        public void FilterAndSum()
+        {
+            var sw = Stopwatch.StartNew();
+            var sum = Enumerable.Range(0, 1000000)
+                                .Where(x => x % 1000 == 0)
+                                .Sum();
+            Console.WriteLine($"{sum}: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
